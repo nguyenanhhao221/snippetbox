26e2b307def4bf2fbf87cd61c0302fb475c80f57
Hao Nguyen <hao@haonguyen.tech>
2024-10-06 03:58:08 +0700

    feat: add flag to control address of the server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Flag to control config of the application
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	flag.Parse()
+
 	// as good practice always use our own ServerMux
 	mux := http.NewServeMux()
+
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
@@ -20,8 +26,8 @@ func main() {
 	mux.HandleFunc(fmt.Sprintf("%s /snippet/{id}", http.MethodGet), snippetView)
 	mux.HandleFunc(fmt.Sprintf("%s /snippet", http.MethodPost), snippetCreate)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
