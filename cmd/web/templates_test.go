5d786a948c717264584410248ee401ea69d331f9
Hao Nguyen <hao@haonguyen.tech>
2024-10-17 12:38:43 +0700

    test(humanDate): extend for zero time and always return UTC

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,11 +6,22 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	tm := time.Date(2024, 10, 17, 3, 41, 59, 0, time.UTC)
-	hd := humanDate(tm)
+	tests := []struct {
+		name   string
+		tm     time.Time
+		expect string
+	}{
+		{name: "UTC", tm: time.Date(2024, 10, 17, 3, 41, 59, 0, time.UTC), expect: "17 Oct 2024 at 03:41"},
+		{name: "Empty", tm: time.Time{}, expect: ""},
+		{name: "CET", tm: time.Date(2024, 10, 17, 3, 41, 59, 0, time.FixedZone("CET", 1*60*60)), expect: "17 Oct 2024 at 02:41"},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			hd := humanDate(tc.tm)
 
-	expect := "17 Oct 2024 at 03:41"
-	if hd != expect {
-		t.Errorf("Got %q, expect %q\n", hd, expect)
+			if hd != tc.expect {
+				t.Errorf("Got %q, expect %q\n", hd, tc.expect)
+			}
+		})
 	}
 }
