5a0a7cee5684fd1864678c3bb4cdd7b70b08dd3b
Hao Nguyen <hao@haonguyen.tech>
2024-12-22 18:41:17 +0700

    feat: add helper test for e2e

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.haonguyen.tech/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication()
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	statusCode, _, body := ts.get(t, "/ping")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
