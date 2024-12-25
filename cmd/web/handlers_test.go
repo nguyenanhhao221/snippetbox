bf70f2fce783885e110800fa51a5df60b3d8b79e
Hao Nguyen <hao@haonguyen.tech>
2024-12-25 17:57:22 +0700

    test: add test for snippet view

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -17,3 +17,54 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, statusCode, http.StatusOK)
 	assert.Equal(t, body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	tests := []struct {
+		name    string
+		urlPath string
+		expCode int
+		expBody string
+	}{
+		{
+			name:    "Valid ID",
+			urlPath: "/snippet/1",
+			expCode: http.StatusOK,
+			expBody: "Bar",
+		},
+		{
+			name:    "Non-existent ID",
+			urlPath: "/snippet/2",
+			expCode: http.StatusNotFound,
+		},
+		{
+			name:    "Negative ID",
+			urlPath: "/snippet/-1",
+			expCode: http.StatusNotFound,
+		},
+		{
+			name:    "Decimal ID",
+			urlPath: "/snippet/1.23",
+			expCode: http.StatusNotFound,
+		},
+		{
+			name:    "String ID",
+			urlPath: "/snippet/foo",
+			expCode: http.StatusNotFound,
+		},
+		{
+			name:    "Empty ID",
+			urlPath: "/snippet/",
+			expCode: http.StatusNotFound,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			statusCode, _, body := ts.get(t, tc.urlPath)
+			assert.Equal(t, statusCode, tc.expCode)
+			assert.StringContains(t, body, tc.expBody)
+		})
+	}
+}
