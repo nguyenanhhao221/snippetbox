07d3e0eda874b174e66810668d0b0e0642a8eef8
Hao Nguyen <hao@haonguyen.tech>
2024-12-28 22:06:07 +0700

    chore: add comment for newTestDB

--- internal/models/testutils_test.go
+++ internal/models/testutils_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+// newTestDB Setup and teardown for a test database to use for our test.
+// Require mysql to run with user, table, database setup correctly
 func newTestDB(t *testing.T) *sql.DB {
 	db_conn_str := "test_web@/test_snippetbox?parseTime=true&multiStatements=true"
 	db, err := sql.Open("mysql", db_conn_str)
